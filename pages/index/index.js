07cf5f1c093c82579eac95f7d78fdad9db1ebc2d
Qiao Zhang <qiao.zhang.qz@outlook.com>
2018-06-05 17:54:06 +0800

    Summary

---
+++ pages/index/index.js
@@ -0,0 +1,59 @@
+// pages/index/index.js
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
+Page({
+  data:{
+    nowTemp: '',
+    nowWeather: '',
+    imageSrc: ''
+  },
+  onPullDownRefresh(){
+    this.getNow(()=>{
+      wx.stopPullDownRefresh()
+    })
+  },
+  onLoad(){
+    this.getNow()
+  },
+  getNow(callback){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '广州市'
+      },
+      success: res => {
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        this.setData({
+          nowTemp: temp + '°',
+          nowWeather: weatherMap[weather],
+          imageSrc: '/images/' + weather + '-bg.png'
+        })
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather]
+        })
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
+  }
+})
